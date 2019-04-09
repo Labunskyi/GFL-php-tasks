86301c0df0d7fb971618b88bc817c7a64f9d24a6
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-09 15:09:30 +0300

    Update SqlDb.php

--- task4/libs/SqlDb.php
+++ task4/libs/SqlDb.php
@@ -3,34 +3,94 @@ class SqlDb
 {
 	
 	protected $table;
-	protected $value; // название значение ячейки
-	protected $values; // название значение ячейки
-	protected $field; // название полей таблицы
-	protected $fields; // массив полей
-	protected $limit; // название лимита
+	protected $value;
+	protected $arrValues = array(); 
+	protected $arrFields = array(); 
+	protected $limit; 
 	
-	
-	public function __construct() {
-		$this->table = '';
-		$this->value = ''; 
-		$this->values = array(); 
-		$this->field = ''; 
-		$this->fields = array(); 
-	}
-	
-	public function setSelect($field, $table, $limit ) {
-		$this->field = $field;
+	public function setTable($table) {
 		$this->table = $table;
-		$this->limit = $limit;
 	}
 	
+	public function getTable() {
+		return $this->table;
+	}
 	
-	public function getSelect() {
+	public function setField($field) {
+		$this->field = $field;
+		array_push($this->arrFields, $field);
+	}
+	
+	public function getField() {
+		$this->strFields = implode(", ", $this->arrFields);
+		return $this->strFields;
+	}
+	
+	public function setValue($value) {
+		$this->velue= $value;
+		array_push($this->arrValues, $value);
+		$res = "";
+		foreach ($this->arrValues as $element ) {
+		$res .= "'".str_replace(' ', "', '",$element )."', ";
+		}
+		$this->substrValues = substr($res,0,-2);
+	}
+	
+	public function getValue() {
+		return $this->substrValues;
+	}
+	
+	public function setLimit($limit) {
+		if (is_int($limit)) {
+			$this->limit = $limit;
+		} else {
+			return "Not an integer";
+		}
+	}
+	
+	public function getLimit() {
+		return $this->limit;
+	}
+	
+	public function setKeyField($keyField) {
+		$this->keyField = $keyField;
+	}
+	
+	public function getKeyField() {
+		return $this->keyField;
+	}
+	
+	public function setKeyValue($keyValue) {
+		$this->keyValue = $keyValue;
+		$str = '';
+		$str .= "'".str_replace(' ', "', '",$this->keyValue )."', ";
+		$this->substrKeyValue = substr($str,0,-2);
 		
-		$query = "SELECT $this->field FROM $this->table LIMIT $this->limit";
-		print_r($query); 
-		return $query;
 	}
 	
+	public function getKeyValue() {
+		print_r($this->substrKeyValue);
+		return $this->substrKeyValue;
+	}
+	
+	public function select() {
+		$this->query = "SELECT {$this->getField()} FROM {$this->getTable()} LIMIT {$this->getLimit()}";
+		return $this->query;
+	}
+	
+	public function insert() {
+		$this->query = "INSERT INTO {$this->getTable()} ({$this->getField()}) VALUES ({$this->getValue()})";
+		print_r($this->query);
+		return $this;
+	}
+	
+	public function delete() {
+		$this->query = "DELETE FROM {$this->getTable()} WHERE {$this->getKeyField()} = {$this->getKeyValue()}";
+		return $this->query;
+	}
+	
+	public function update() {
+		$this->query = "UPDATE {$this->getTable()} SET {$this->getField()} = {$this->getValue()} WHERE {$this->getKeyField()} = {$this->getKeyValue()}";
+		return $this->query;
+	}
 }
-?>
