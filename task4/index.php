5598f682376c0d98120a7276fcfcf3a22a77e536
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-09 15:08:39 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -1,20 +1,31 @@
 <?php
-
 include 'config.php';
 include 'libs/SqlDb.php';
 include 'libs/MysqlDb.php';
+include 'libs/PostgreDb.php';
 include 'libs/PdoDb.php';
 
+$PdoDb = new PdoDb(USER, PASSWORD);
 
-$SqlDb = new SqlDb();
+$MysqlDb = new MysqlDb(HOST, USER, PASSWORD, DB_NAME);
 
-$SqlDb->setSelect('alias', 'books', '1' );
-$SqlDb->getSelect();
-$PdoDb = new PdoDb(HOST, USER, PASSWORD, DB_NAME);
+$PostgreDb = new PostgreDb(HOST, USER, PASSWORD, DB_NAME);
 
+//print_r($PdoDb->select()->setField('alias')->setField('title')->getField()->from('books')->setLimit(1)->exeSelect());
 
+$PdoDb->setTable('category');
+$PdoDb->getTable();
+$PdoDb->setField('id_category');
+$PdoDb->setField('name_category');
+$PdoDb->getField();
+$PdoDb->setValue(120);
+$PdoDb->setValue('name');
+$PdoDb->getValue();
 
-print_r($PdoDb->select()->connect());
+$PdoDb->setKeyValue('names');
+$PdoDb->getKeyValue();
+
+$PdoDb->insert()->connect();
 
 //include 'templates/index.php';
 
