2a3eab8ec0f959a223ba306834f3a5bd37514046
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-09 15:08:57 +0300

    Update index.php

--- task4/index.php
+++ task4/index.php
@@ -11,8 +11,6 @@ $MysqlDb = new MysqlDb(HOST, USER, PASSWORD, DB_NAME);
 
 $PostgreDb = new PostgreDb(HOST, USER, PASSWORD, DB_NAME);
 
-//print_r($PdoDb->select()->setField('alias')->setField('title')->getField()->from('books')->setLimit(1)->exeSelect());
-
 $PdoDb->setTable('category');
 $PdoDb->getTable();
 $PdoDb->setField('id_category');
