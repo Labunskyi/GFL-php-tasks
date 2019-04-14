fa1e21c1ff74351ca192e0aa25aa902bec91c610
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-14 21:17:22 +0300

    Update Musician.php

--- task6/libs/Musician.php
+++ task6/libs/Musician.php
@@ -1,23 +1,37 @@
 <?php
 class Musician implements iMusician
 {
+	private $nameBand;
+	private $musicianType;
+	
     public function addInstrument(iInstrument $obj)
     {
-        $this->arrValues, $value;
+        $this->addInstrument = $obj;
     }
 
     public function getInstrument()
     {
-
+		return $this->addInstrument;
+		
     }
 
     public function assingToBand(iBand $nameBand)
     {
-
+		$this->nameBand = $nameBand;
     }
-
+	
+	public function getAssingToBand()
+    {
+		return $this->nameBand;
+    }
+	
+	public function setMusicianType($musicianType)
+    {
+		$this->musicianType = $musicianType;
+    }
+	
     public function getMusicianType()
     {
-
+		return $this->musicianType;
     }
 }
