6de8c3640de147b95615ee7c991f9e3d6129eef0
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-14 21:15:04 +0300

    Add files via upload

---
+++ task6/templates/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+</head>
+<body>
+<?php
+echo $mbandName;
+echo"<br/>";
+echo $mbandGenre;
+echo"<br/>";
+echo $mbandMusicians;
+echo"<br/>";
+
+echo $scorpionsName;
+echo"<br/>";
+echo $scorpionsGenre;
+echo"<br/>";
+echo $scorpionsMusicians;
+
+?>
+</body>
+</html>
