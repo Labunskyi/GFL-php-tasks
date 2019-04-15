a1a3ee4f35427479924968292c0f2ad5f99cc17c
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-15 16:05:42 +0300

    Update SqlClass.php.php

--- task5/libs/SqlClass.php.php
+++ task5/libs/SqlClass.php.php
@@ -1,3 +1,41 @@
 <?php
+class SqlClass implements iWorkData
+{
+	
+	protected $connection;
+	protected $query;	
+		
+	public function __construct() {
+		$this->connection = new PDO ("mysql:host=localhost;dbname=mvc;charset=utf8", 'root', '');
+		
+	}
+  
+    public function saveData($key, $val)
+    {
+		$this->field = $key;
+		$this->value = $val;
+		$str = '';
+		$str .= "'".str_replace(' ', "', '",$this->value )."', ";
+		$this->substrValue = substr($str,0,-2);
+		
+		$this->query = "INSERT INTO category ({$this->field}) VALUES ({$this->substrValue})";
+		$result = $this->connection->query($this->query) ;
+		return $result;
+    }
 
-?>
+    public function getData($key)
+    {	
+		$this->field = $key;
+		$this->query = "SELECT {$this->field} FROM category";
+		$result = $this->connection->query($this->query) ;
+		return $result;
+    }
+
+    public function deleteData($key)
+    {	
+		$this->field = $key;
+        $this->query = "ALTER TABLE categories DROP COLUMN {$this->field}";
+		$result = $this->connection->query($this->query) ;
+		return $result;
+    }
+}
