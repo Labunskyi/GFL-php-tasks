0335d2355ae04fa332275ed765bd722792bd7082
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-15 16:04:06 +0300

    Update IniClass.php.php

--- task5/libs/IniClass.php.php
+++ task5/libs/IniClass.php.php
@@ -1,42 +1,44 @@
 <?php
-include 'libs/iWorkData.php';
-include 'libs/SessionClass.php';
-include 'libs/CookieClass.php';
-include 'libs/IniClass.php';
-include 'libs/JsonClass.php';
-include 'libs/SqlClass.php';
+class IniClass implements iWorkData
+{
+	
+	protected $parse = array();
+	protected $saveData = array();	
+  
+    public function saveData($key, $val)
+    {	
+		$this->saveData[$key] = $val;
+		
+		$str = '';
+		foreach ($this->saveData as $key => $value) {
+			$str = $key . '=' . $value;
+		}
+		file_put_contents(INI_PATH, PHP_EOL, FILE_APPEND);
+		file_put_contents(INI_PATH, $str, FILE_APPEND);
+    }
 
+    public function getData($key)
+    {	
+		$this->parse = parse_ini_file(INI_PATH);
+		if(isset($this->parse[$key])){
+			$this->parse[$key];
+			return $this->parse[$key];
+		}
+		return null;
+    }
 
-$session = new SessionClass();
-$session->saveData('session', 4);
-$cookie = new CookieClass();
-$cookie->saveData('cookie', 4);
-//$ini = new IniClass();
-//$json = new JsonClass();
-//$sql = new SqlClass();
-
-
-function saveData(iWorkData $obj){
-    $obj->saveData($key, $val);
+    public function deleteData($key)
+    {	
+		$this->parse = parse_ini_file(INI_PATH);
+		if(isset($this->parse[$key])){
+            unset($this->parse[$key]);
+			$str = '';
+				foreach ($this->parse as $key => $value) {
+				$str .= $key . '=' . $value . PHP_EOL;
+			}
+			file_put_contents(INI_PATH, $str);
+            return true;
+        }
+        return null;
+    }
 }
-
-function getData(iWorkData $obj){
-    $obj->getData($key);
-}
-
-function deleteData(iWorkData $obj){
-    $obj->deleteData($key);
-}
-
-saveData($session);
-getData($session);
-deleteData($session);
-
-saveData($cookie);
-getData($cookie);
-deleteData($cookie);
-
-
-
-//include 'templates/index.php';
-?>
