5975f77fa7c10ab4c526956f84ce7b56c81b67b3
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-15 16:02:41 +0300

    Add files via upload

---
+++ task5/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+define('INI_PATH', dirname(__FILE__).DS.'php.ini');
+define('JSON_PATH', dirname(__FILE__).DS.'php.json');
+?>
---
+++ task5/templates/index.php
@@ -0,0 +1,28 @@
+<!DOCTIPE html>
+<html>
+<header>
+</header>
+<body>
+<?php
+print_r($printGetSession);
+echo "<br/>";
+print_r($printDelSession);
+echo "<br/>";
+print_r($printGetCookie);
+echo "<br/>";
+print_r($printDelCookie);
+echo "<br/>";
+print_r($printGetSql);
+echo "<br/>";
+print_r($printDelSql);
+echo "<br/>";
+print_r($printGetIni);
+echo "<br/>";
+print_r($printDelIni);
+echo "<br/>";
+print_r($printGetJson);
+echo "<br/>";
+print_r($printDelJson);
+?>
+</body>
+</html>
