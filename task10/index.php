1bfd56dcb97b174e61dbaad27249226f084166d7
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-16 19:57:23 +0300

    Update index.php

--- task10/index.php
+++ task10/index.php
@@ -1,41 +1,10 @@
 <?php
-
 include 'config.php';
 include 'libs/SqlDb.php';
-//include 'libs/MysqlDb.php';
 include 'libs/PdoDb.php';
 
+$PdoDb = new PdoDb(USER, DB_NAME);
 
-
-
-$PdoDb = new PdoDb(HOST, USER, PASSWORD, DB_NAME);
-$PdoDb->setField('alias');
-$PdoDb->setField('title');
-$PdoDb->getField();
-
-$PdoDb->setValue('102');
-$PdoDb->setValue('101');
-$PdoDb->getValue();
-
-$PdoDb->setTable('category');
-$PdoDb->getTable();
-
-$PdoDb->setLimit(2);
-$PdoDb->getLimit();
-
-$PdoDb->setKeyField('id');
-$PdoDb->getKeyField();
-
-$PdoDb->setKeyValue(2);
-$PdoDb->getKeyValue();
-
-
-
-//print_r($PdoDb->select()->connect());
-//print_r($PdoDb->insert()->connect());
-//print_r($PdoDb->delete()->connect());
-//print_r($PdoDb->update()->connect());
-
-//include 'templates/index.php';
+print_r($PdoDb->select()->setField('name')->setField('pwd')->from()->setTable('user')->connect());
 
 ?>
