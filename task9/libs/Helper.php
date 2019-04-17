f98d39f03ef972e70b019f4101de019aa0dc7e54
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-17 15:56:45 +0300

    Update Helper.php

--- task9/libs/Helper.php
+++ task9/libs/Helper.php
@@ -1,7 +1,6 @@
 <?php
 class Helper
 {
-	public $arr = array();
 	
     public function list($teg, $li, $arr)
 	{	
@@ -9,10 +8,59 @@ class Helper
 		foreach($arr as $val) {
 			$arr2 .= '<' . $li . '>' . $val . '</' . $li . '>';
 		}
+		$list = '<' . $teg . '>' . $arr2 . '</' . $teg . '>';
+		return $list;
+	}
+	
+	public function termin($teg, $dt, $dd, $arr)
+	{	
+		$arr2 = '';
+		foreach($arr as $key => $val) {
+			$arr2 .= '<' . $dt . '>' . $key . '</' . $dt . '>' . '<' . $dd . '>' . $val . '</' . $dd . '>';
+		}
 		$list = '<' . $teg . '>' . $arr2 . '<' . $teg . '>';
 		return $list;
 	}
 	
+	public function select($teg, $option, $arr)
+	{
+		$arr2 = '';
+		foreach($arr as $val) {
+			$arr2 .= '<' . $option . '>' . $val . '</' . $option . '>';
+		}
+		$list = '<' . $teg . ' multiple>' . $arr2 . '</' . $teg . '>';
+		return $list;
+	}
 	
+	public function radio($teg, $input, $arr)
+	{
+		$arr2 = '';
+		foreach($arr as $val) {
+			$arr2 .= '<' . $input . ' type="radio" name="group1">' . $val . '<br>';
+		}
+		$list = '<' . $teg . '>' . $arr2 . '</' . $teg . '>';
+		return $list;
+	}
+	
+	public function check($teg, $input, $arr)
+	{
+		$arr2 = '';
+		foreach($arr as $val) {
+			$arr2 .= '<' . $input . ' type="checkbox">' . $val . '<br>';
+		}
+		$list = '<' . $teg . '>' . $arr2 . '</' . $teg . '>';
+		return $list;
+	}
+	
+	public function table($teg, $input, $arr)
+	{
+		$arr2 = '';
+		foreach($arr as $val) {
+			$arr2 .= '<' . $input . ' type="checkbox">' . $val . '<br>';
+		}
+		$list = '<' . $teg . '>' . $arr2 . '</' . $teg . '>';
+		return $list;
+	}
 	
 }
+
