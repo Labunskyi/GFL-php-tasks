649385a6947c26ca38f8cfcb7583067b9bda47bf
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-04-21 18:23:51 +0300

    Update index.php

--- task10/templates/index.php
+++ task10/templates/index.php
@@ -1,16 +1,5 @@
  <?php
 
-print_r($fr->readFileByRow());
-echo "</br>";
-echo "</br>";
-print_r($fr->readFileBySymbol());
-echo "</br>";
-echo "</br>";
-$fr->setChangeByRow(1, 'fff as');
-print_r($fr->getChangeByRow(1));
-echo "</br>";
-echo "</br>";
-$fr->setChangeBySymbol(1, 2, 't');
-print_r($fr->getChangeBySymbol(1, 2));
+
 
 ?>
