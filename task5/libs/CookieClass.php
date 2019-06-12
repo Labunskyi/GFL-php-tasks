7869d88ee3fdec07ec0870dcdb8d09649a6d8ecb
Labunskyi Volodymyr <labunskyi@rambler.ru>
2019-06-12 18:20:31 +0300

    Update CookieClass.php

--- task5/libs/CookieClass.php
+++ task5/libs/CookieClass.php
@@ -6,20 +6,20 @@ class CookieClass implements iWorkData
         setcookie($key, $val, time() +36);
         $_COOKIE[$key] = $val;
     }
-
     public function getData($key)
     {
         if(isset($_COOKIE[$key])){
-			setcookie($key, "", time() -36);
             return $_COOKIE[$key];
         }
-		return null;
+	return null;
     }
-
     public function deleteData($key)
     {
         if(isset($_COOKIE[$key])){
+            setcookie($key, "", time() -36);
             unset($_COOKIE[$key]);
+            return true;
         }
+        return null;
     }
 }
